df38af5a1c0a8656b6795da80846773b535ca854
ntsum <natsukajiwaram@gmail.com>
2019-04-12 12:10:06 -0400

    mvp complete, added styling from previous smurf sprint

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getSmurfs } from "../actions";
+import Smurf from "./Smurf";
+import SmurfForm from "./SmurfForm";
+import "./App.css";
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +12,36 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>SMURFS 2.0 W/ Redux</h1>
+        <p>
+          {this.props.smurfs.map(smurf => {
+            return <Smurf smurf={smurf} key={smurf.id} id={smurf.id} />;
+          })}
+        </p>
+        <SmurfForm />
       </div>
     );
   }
 }
 
-export default App;
+function mstp(state) {
+  return {
+    smurfs: state.smurfs,
+    fetchSmurfs: state.fetchSmurfs,
+    addSmurf: state.addSmurf,
+    updatingSmurf: state.updatingSmurf,
+    error: state.error
+  };
+}
+
+export default connect(
+  mstp,
+  { getSmurfs }
+)(App);
